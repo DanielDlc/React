142c5c22e3bc0d675a31234cce6afd0e291b8679
DanielDlc <dlc.engcomputacao@gmail.com>
2021-08-02 00:00:16 -0300

    lembrando o básico de js

---
+++ javaScript/01_resumo.js
@@ -0,0 +1,24 @@
+// declarando uma variável
+var qualquerNumero = 10;
+console.log(qualquerNumero);
+
+// Tipo de dados Objeto
+var pessoa = {
+  altura: 1.89,
+  peso: 91,
+};
+console.log(pessoa);
+console.log(pessoa.altura);
+console.log(pessoa.peso);
+
+// tipos de dados array
+//             1° 2° 3° 4° ...
+var numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+console.log(numeros);
+
+//testar terceira posição n°4
+console.log(
+  "O quarto número da esquerda para direita é o número: ",
+  numeros[3]
+);

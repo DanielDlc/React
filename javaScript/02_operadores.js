ff689f9292eac7fbf15bfb7fd6cfe73a48575902
DanielDlc <dlc.engcomputacao@gmail.com>
2021-08-02 11:36:19 -0300

    operadores

---
+++ javaScript/02_operadores.js
@@ -0,0 +1,46 @@
+// operadores Aritméticos
+
+var adicao = 1 + 5;
+console.log("adição:", adicao);
+
+var subtracao = 10 - 5;
+console.log("subtração:", subtracao);
+
+var multiplicacao = 10 * 5;
+console.log("multiplicação:", multiplicacao);
+
+var divisao = 100 / 10;
+console.log("divisão:", divisao);
+
+console.log();
+
+// Operadores Aritméticos Abreviados
+
+adicao = adicao + 1;
+console.log("valor anterior da adição + 1 =", adicao);
+
+adicao++;
+console.log("valor anterior da adição + 1 =", adicao);
+
+console.log();
+
+subtracao--;
+console.log("valor anterior da subtracão - 1 =", subtracao);
+
+console.log();
+
+// podemos representar adicao = adicao + 20 ou adicao +=20
+adicao = adicao + 20;
+console.log("somar + 20 números acima do anterior", adicao);
+
+adicao += 72;
+console.log("somar + 72 números:", adicao);
+
+console.log();
+
+// multiplicar valor
+multiplicacao = multiplicacao * 10;
+console.log("multiplicação =", multiplicacao);
+
+multiplicacao *= 2;
+console.log("valor anterior da multiplicação x 2 =", multiplicacao);

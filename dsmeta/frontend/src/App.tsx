4c16b23a5c6dfad3efe7feb61297d3d55930821c
DanielDlc <dlc.engcomputacao@gmail.com>
2022-08-01 16:22:54 -0300

    app com confirmação de envio de sms

--- dsmeta/frontend/src/App.tsx
+++ dsmeta/frontend/src/App.tsx
@@ -1,13 +1,16 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from './components/Header'
 import SalesCard from './components/SalesCard'
 
 function App() {
   return(
     <>
+    <ToastContainer />
       <Header />
       <main>
         <section id="sales">
-          <div class="dsmeta-container">
+          <div className="dsmeta-container">
             <SalesCard/>
           </div>
         </section>
